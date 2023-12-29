251574a007ef2b8b56ae128657672c49d05b7bef
James Scott <scott.jams@gmail.com>
2023-12-29 14:12:20 -0500

    change directory

--- gui/views/skeleton.vue
+++ gui/views/skeleton.vue
@@ -1,11 +1,9 @@
 <script setup>
-import { useSampleSkeletonStore } from "../stores/sampleSkeletonStore.js";
 import { ref, inject } from "vue";
     let name = ref("skeleton");
     let description = ref("skeleton description");
     let sampleVariable = ref("");
     const $api = inject("$api");
-    const sampleSkeletonStore = useSampleSkeletonStore();
     function testApi(){
         // Sample API request
         $api.get("/api/v2/health").then((response) => {
@@ -24,7 +22,5 @@ h2 {{ name }}
 p {{ description }}
 button.button.fancy-button.is-fullwidth(type="button" @click="testApi()") Test API 
 button.button.fancy-button.is-fullwidth(type="button" @click="sampleFunction()") Click Me
-button.button.fancy-button.is-fullwidth(type="button" @click="sampleSkeletonStore.changeStoreVariable()") Change store variable
 p {{ sampleVariable }}
-p {{ sampleSkeletonStore.sampleStoreVariable }}
 </template>
